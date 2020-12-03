7a07f2c91d6d7edbb05b395dd31291977f6e27b2
vehikl <vehikl@vehikl.com>
2020-12-03 13:48:52 -0500

    Add typescript

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,9 +4,11 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+import Vue from 'vue';
+
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
 /**
  * The following block of code may be used to automatically register your
