12fcc98c2021d64e42b3c18e3f9a2b5845070ebf
vehikl <vehikl@vehikl.com>
2020-12-10 15:02:03 -0500

    Remove clutter

--- resources/js/components/foobar.spec.ts
+++
@@ -1,7 +0,0 @@
-import Foobar from "./Foobar";
-
-describe("Foobar", () => {
-   it("Returns hi whenever we ask it to say Hi", () => {
-      expect((new Foobar()).sayHi()).toEqual("Hi");
-   });
-});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>{{ config('app.name') }}</title>
 
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="antialiased">
-        <div id="app">
-            <example-component></example-component>
-        </div>
-    </body>
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+<body>
+<div id="app">
+    HELLO
+</div>
+</body>
 </html>
